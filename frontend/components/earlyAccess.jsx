c8f9e90ebfe1132ed07cca2499986038be72968d
Lekhan11 <senthillekhan2005@gmail.com>
2026-01-17 20:01:25 +0530

    backend db changed

--- frontend/components/earlyAccess.jsx
+++ frontend/components/earlyAccess.jsx
@@ -7,7 +7,7 @@ export default function EarlyAccess({cancel}) {
 
   const submit = async () => {
   try {
-    const res = await fetch("http://127.0.0.1:8000/users", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/users`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email }),
